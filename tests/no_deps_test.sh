912b2157cc18e8f2ccb51e71012c402564ca5dec
Fabian Meumertzheim <fabian@meumertzhe.im>
2023-12-09 20:08:26 +0100

    Test for no non-dev dependencies

---
+++ tests/no_deps_test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Verify that the buildozer module has no non-dev dependencies.
+
+set -euo pipefail
+
+# Fail on lines that don't end with " True".
+! $BUILDOZER 'print name dev_dependency' //MODULE.bazel:%bazel_dep \
+    | grep -v ' True$'
